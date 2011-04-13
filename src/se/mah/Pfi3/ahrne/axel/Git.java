ac39f5f8437b93a3bcec30b8e2ab91648dd23ed9
ahrne89 <axel.ahrne@gmail.com>
2011-04-13 19:42:38 +0200

    Signed-off-by: ahrne89 <axel.ahrne@gmail.com>

---
+++ src/se/mah/Pfi3/ahrne/axel/Git.java
@@ -0,0 +1,13 @@
+package se.mah.Pfi3.ahrne.axel;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Git extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
